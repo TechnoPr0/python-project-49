31a6342a6e8b434ccec5469197e0296bbf205527
Kirill_S <kiris2004@mail.ru>
2023-09-19 01:13:58 +0300

    moved the function 'even' in folder games

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
-from random import randint
+
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_games import hello
+from brain_games.games.brain_even import even
 
 
 def main():
     hello()
     name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    count_correct_answer = 0
-    while count_correct_answer < 3:
-        random_number = randint(1, 100)
-        if random_number % 2 == 0:
-            correct_answer = 'yes'
-        elif random_number % 2 != 0:
-            correct_answer = 'no'
-        print(f"Question: {random_number}")
-        user_answer = input('Your answer: ')
-        if correct_answer == user_answer.lower():
-            print("Correct!")
-            count_correct_answer += 1
-        else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            print(f"Let's try again, {name}!")
-            break
-    if count_correct_answer == 3:
-        print(f"Congratulations, {name}!")
+    even(name)
+
+
+if __name__ == '__main__':
+    main()
